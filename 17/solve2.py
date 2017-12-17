3f8229387f6d537016d348c93bc3e0ea4977f0cc
Tanner Stephens <tannerstephens@0x99.net>
2017-12-16 23:34:20 -0600

    Day 17 - Tweak

--- 17/solve2.py
+++ 17/solve2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 puzzle = 349
-l = 1
 pos = 0
 for i in range(1,50000001):
-    pos = ((puzzle + pos) % l)+1
-    l += 1
+    pos = ((puzzle + pos) % i)+1
     if pos == 1:
         val = i
 
