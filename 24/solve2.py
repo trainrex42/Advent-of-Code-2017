d1087bedc10c06c3c4a7fb05350423b648ad9afc
Tanner Stephens <tannerstephens@0x99.net>
2017-12-23 23:43:00 -0600

    Day 24

---
+++ 24/solve2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+def rem(l,r):
+    o = l[:]
+    o.remove(r)
+    return o
+
+def strength(br):
+    s = 0
+
+    for seg in br:
+        s += seg[1]
+
+    return s
+
+def make_bridge(seg,look,br=None):
+    if br == None:
+        br = []
+    
+    best = None
+    best_s = 0
+    best_l = 0
+    for s in seg:
+        if look in s[0]:
+            l = s[0][(s[0].index(look)+1)%2]
+            cur = make_bridge(rem(seg,s),l,br + [s])
+            l = len(cur)
+            if l > best_l:
+                best = cur[:]
+                best_s = strength(cur)
+                best_l = l
+            elif l == best_l:
+                s = strength(cur)
+                if s > best_s:
+                    best = cur[:]
+                    best_s = s
+                    best_l = l
+
+    if best == None:
+        return br
+    else:
+        return best
+    
+
+
+with open("input.txt") as f:
+    data = [i.strip() for i in f.readlines()]
+
+segments = []
+
+for line in data:
+    w = line.split("/")
+    segments.append([[int(w[0]), int(w[1])], int(w[0]) + int(w[1])])
+
+bridge = make_bridge(segments,0)
+
+print(strength(bridge))
+
+
+
+        
